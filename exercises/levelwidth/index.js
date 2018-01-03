9a851334c9c3092f84b20ebe0ce711d9aac62d98
Colin McNamara <coolinmc6@gmail.com>
2018-01-03 10:20:21 -0500

    Finish L118

--- exercises/levelwidth/index.js
+++ exercises/levelwidth/index.js
@@ -11,6 +11,48 @@
 // 4       5
 // Answer: [1, 3, 2]
 
-function levelWidth(root) {}
+// CM Solution #1 => passes tests
+// function levelWidth(root) {
+// 	let counter = [];
+// 	let working = [];
+// 	working.push(root);		// I can just do => let working = [root, 's']; I don't have to push elements
+// 	counter.push(0);
+// 	working.push('s');
+// 	while(working.length > 1) {
+// 		let node = working.shift();
+		
+// 		if(node.children) {
+// 			working.push(...node.children);
+// 			counter[counter.length-1]++
+// 		} else if(node == 's' && working.length == 0) {
+// 			return;
+// 		} else if(node == 's' && working.length > 0) {
+// 			working.push('s');
+// 			counter.push(0);
+// 		}
+// 	}
+// 	return counter;
 
+// }
+
+
+function levelWidth(root) {
+	const arr = [root, 's'];
+	const counters = [0];
+
+	while(arr.length > 1) {
+		const node = arr.shift();
+
+		if(node === 's') {
+			counters.push(0);
+			arr.push('s')
+		} else {
+			arr.push(...node.children);
+			counters[counters.length-1]++;
+		}
+	}
+
+	return counters;
+
+}
 module.exports = levelWidth;
